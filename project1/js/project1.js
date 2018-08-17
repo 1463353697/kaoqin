7c9d1bed1c1532cd07f02fc050ab649df79e37c2
1463353697 <1463353697@qq.com>
2018-08-17 13:37:25 +0800

    first add $.post

--- project1/js/project1.js
+++ project1/js/project1.js
@@ -27,3 +27,60 @@ function navLetf() {
 	}
 }
 navLetf();
+
+
+
+//请假审核
+// var $applyHolidayIf = $("#applyholidayif");
+// $applyHolidayIf.css("display" , "block");
+
+
+
+//下面这两个操作可以封装为一个函数
+
+//获取请假审核表格单位
+window.onload = function(){
+var applyHolidayTab = document.getElementById('applyholidaytab')
+.getElementsByTagName("tr");
+//给每个单位表格添加点击事件，点击之后，还要把各种信息添加进去，
+//还要实现同意、暂不处理、驳回的处理结果，并关闭信息页面
+for (var i = 0; i < applyHolidayTab.length; i++) {
+	applyHolidayTab[i].onclick = function(){
+		var $applyHolidayIf = $("#applyholidayif");
+		$applyHolidayIf.css("display" , "block");
+
+	}
+}
+//获得出差审核单位表格
+var applyOutTab = document.getElementById('applyouttab')
+.getElementsByTagName('tr');
+//给每个表格添加点击事件
+for (var i = 0; i < applyOutTab.length; i++) {
+	applyOutTab[i].onclick = function(){
+		var $applyOutIf = $("#applyoutif");
+		$applyOutIf.css("display","block");
+	}
+}
+
+//这里是一个封装失败的函数
+// function applyTabClick (applyTabId,applyIfId){
+// 	var applyTab = document.getElementById('applyTabId').getElementsByTagName('tr');
+
+// 	for (var i = 0; i < applyTab.length; i++) {
+// 		applyTab[i].onclick = function(){
+// 			var $applyIf = $("#applyIfId");
+// 			$applyIf.css("display" , "block");
+// 		}
+// 	}
+// }
+// applyTabClick(applyholidaytab,applyholidayif);
+// applyTabClick(applyouttab,applyoutif);
+
+
+
+
+
+
+
+}
+
