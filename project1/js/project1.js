9e5db0b3bee69a6cbf85ad54d870173779a69c00
1463353697 <1463353697@qq.com>
2018-09-02 10:30:48 +0800

    9.2 end

--- project1/js/project1.js
+++ project1/js/project1.js
@@ -40,12 +40,39 @@ navLetf();
 
 
 window.onload = function(){
+
+	//给不同的处理结果添加不一样的颜色
+	function addColor (theTab,get1,class1,get2,class2,get3,class3) {
+		var theTd = theTab.getElementsByTagName('td');
+		for (var i = 0; i < theTd.length; i++) {
+			if (theTd[i].innerHTML == get1) {
+				theTd[i].className = class1;
+			}
+			if (theTd[i].innerHTML == get2) {
+				theTd[i].className = class2;
+			}
+			if (theTd[i].innerHTML == get3) {
+				theTd[i].className = class3;
+			}
+		}
+	}
+
+	//给表格偶数行添加阴影背景颜色
+	function addBackColor (theTab) {
+		var theTr = theTab.getElementsByTagName('tr');
+		for (var i = 0; i < theTr.length; i++) {
+			if(i%2 == 1) {
+			theTr[i].className = "appro_table_2";
+			}
+		}
+
+	}
 	//获取审核表格单位
-	var applyHolidayTab = document.getElementById('applyholidaytab')
-	.getElementsByTagName("tr");
-	var applyOutTab = document.getElementById('applyouttab')
-	.getElementsByTagName('tr');
-//获取显示详细信息的input表单
+	var applyHoliday  =  document.getElementById('applyholidaytab');
+	var applyHolidayTab = applyHoliday.getElementsByTagName("tr");
+	var applyOut = document.getElementById('applyouttab');
+	var applyOutTab = applyOut.getElementsByTagName('tr');
+	//获取显示详细信息的input表单
 	var holidayIfInput = document.getElementById('applyholidayif').getElementsByTagName('input');
 	var outIfInput = document.getElementById('applyoutif').getElementsByTagName('input');
 	//获取显示详细信息的整个页面
@@ -56,6 +83,9 @@ window.onload = function(){
 	var outResult = document.getElementById('out_check_result').getElementsByTagName('button');
     //获取是否紧急的按钮
 	var jinjiIf = document.getElementById('jinji');
+
+	addBackColor(applyHoliday);
+	addBackColor(applyOut);
 
 
 //这是一个测试的语句
@@ -121,7 +151,7 @@ window.onload = function(){
 // 					input[3].value = review[i].startTime;
 // 					input[4].value = review[i].endTime;
 // 					input[5].value = review[i].applyType;
-//					jinjiIf.checked = holidayReview[i].jijin;
+// 					jinjiIf.checked = holidayReview[i].jijin;
 // 				//给表示处理结果的按钮添加点击事件，返回处理结果并关闭页面
 				
 // 					result[0].onclick = function(){
@@ -222,67 +252,42 @@ window.onload = function(){
 // applyTabClick(applyouttab,applyoutif);
 
 
-//给不同的处理结果添加不一样的颜色
-function addColor (theTab,get1,class1,get2,class2,get3,class3) {
-	var theTd = theTab.getElementsByTagName('td');
-	for (var i = 0; i < theTd.length; i++) {
-		if (theTd[i].innerHTML == get1) {
-			theTd[i].className = class1;
-		}
-		if (theTd[i].innerHTML == get2) {
-			theTd[i].className = class2;
-		}
-		if (theTd[i].innerHTML == get3) {
-			theTd[i].className = class3;
-		}
-	}
-}
 
-//给表格偶数行添加阴影背景颜色
-function addBackColor (theTab) {
-	var theTr = theTab.getElementsByTagName('tr');
-	for (var i = 0; i < theTr.length; i++) {
-		if(i%2 == 1) {
-		theTr[i].className = "appro_table_2";
-		}
-	}
-
-}
 
 
 
 //申请进度
 
-var applyProTab = document.getElementById('appro_table');
-var applyProCnetent = "<tr>" +
+	var applyProTab = document.getElementById('appro_table');
+	var applyProCnetent = "<tr>" +
                         "<td>2018-05-05</td>" +
                         "<td>事假</td>"+
                         "<td>驳回</td>" +
                         "<td>admin</td>" +
-                    "</tr>"+
-                    "<tr>" +
+                   	"</tr>"+
+                   	 "<tr>" +
                         "<td>2018-05-05</td>" +
                         "<td>事假</td>"+
                         "<td>未处理</td>" +
                         "<td>admin</td>" +
-                    "</tr>";
-$(applyProTab).append(applyProCnetent);
+                    	"</tr>";
+	$(applyProTab).append(applyProCnetent);
 
-addColor(applyProTab,"驳回","appro_situation_oppose","同意","appro_situation_agree","未处理","appro_situation_wait");
-addBackColor(applyProTab);
+	addColor(applyProTab,"驳回","appro_situation_oppose","同意","appro_situation_agree","未处理","appro_situation_wait");
+	addBackColor(applyProTab);
 
-//打卡情况
-var cardStuaTab = document.getElementById('stua_cardtb');
-var cardStuaCnetent = "<tr>" +
-                        "<td>2" +
+	//打卡情况
+	var cardStuaTab = document.getElementById('stua_cardtb');
+	var cardStuaCnetent = "<tr>" +
+                        "<td>2</td>" +
                         "<td>2018-05-05</td>"+
                         "<td>admin</td>" +
                         "<td>09:38:11</td>" +
                         "<td>18:30:15</td>" +
                         "<td>迟到</td>"+
-                    "</tr>"+
+                   	"</tr>"+
                     "<tr>" +
-                        "<td>2" +
+                        "<td>2</td>" +
                         "<td>2018-05-05</td>"+
                         "<td>admin</td>" +
                         "<td>09:38:11</td>" +
@@ -290,18 +295,83 @@ var cardStuaCnetent = "<tr>" +
                         "<td>旷工</td>"+
                     "</tr>"+
                     "<tr>" +
-                        "<td>2" +
+                        "<td>2</td>" +
                         "<td>2018-05-05</td>"+
                         "<td>admin</td>" +
                         "<td>09:38:11</td>" +
                         "<td>18:30:15</td>" +
                         "<td>旷工 迟到</td>"+
                     "</tr>";
-$(cardStuaTab).append(cardStuaCnetent);
-addBackColor(cardStuaTab);
-addColor(cardStuaTab,"迟到","be_late","旷工","stay_away","旷工 迟到","stay_away");
+	$(cardStuaTab).append(cardStuaCnetent);
+	addBackColor(cardStuaTab);
+	addColor(cardStuaTab,"迟到","be_late","旷工","stay_away","旷工 迟到","stay_away");
 
+	//请假情况
+	var hliStuaTab = document.getElementById('hli_stuatb');
+	var hliStuaContent = "<tr>" +
+                        "<td>2018-05-05</td>" +
+                        "<td>6</td>"+
+                        "<td>admin</td>" +
+                        "<td>病假</td>" +
+                        "<td>2018-08-04</td>" +
+                        "<td>2018-08-04</td>"+
+                        "<td>驳回</td>"+
+                    "</tr>"+
+                    "<tr>" +
+                        "<td>2018-05-05</td>" +
+                        "<td>4</td>"+
+                        "<td>admin</td>" +
+                        "<td>病假</td>" +
+                        "<td>2018-08-04</td>" +
+                        "<td>2018-08-04</td>"+
+                        "<td>未处理</td>"+
+                    "</tr>"+
+                    "<tr>" +
+                        "<td>2018-05-05</td>" +
+                        "<td>5</td>"+
+                        "<td>admin</td>" +
+                        "<td>病假</td>" +
+                        "<td>2018-08-04</td>" +
+                        "<td>2018-08-04</td>"+
+                        "<td>通过</td>"+
+                    "</tr>";
 
+	$(hliStuaTab).append(hliStuaContent);
+	addBackColor(hliStuaTab);
+	addColor(hliStuaTab,"驳回","ap_situation_oppose","未处理","ap_situation_wait","通过","ap_situation_agree");
+	//出差情况
+	var outStuaTab = document.getElementById('out_stuatb');
+	var outStuaContent = "<tr>" +
+                        "<td>2018-05-05</td>" +
+                        "<td>6</td>"+
+                        "<td>admin</td>" +
+                        "<td>北京</td>" +
+                        "<td>2018-08-04</td>" +
+                        "<td>2018-08-04</td>"+
+                        "<td>驳回</td>"+
+                    "</tr>"+
+                    "<tr>" +
+                        "<td>2018-05-05</td>" +
+                        "<td>4</td>"+
+                        "<td>admin</td>" +
+                        "<td>南京</td>" +
+                        "<td>2018-08-04</td>" +
+                        "<td>2018-08-04</td>"+
+                        "<td>未处理</td>"+
+                    "</tr>"+
+                    "<tr>" +
+                        "<td>2018-05-05</td>" +
+                        "<td>5</td>"+
+                        "<td>admin</td>" +
+                        "<td>上海</td>" +
+                        "<td>2018-08-04</td>" +
+                        "<td>2018-08-04</td>"+
+                        "<td>通过</td>"+
+                    "</tr>";
+
+	$(outStuaTab).append(outStuaContent);
+	addBackColor(outStuaTab);
+	addColor(outStuaTab,"驳回","ap_situation_oppose","未处理","ap_situation_wait","通过","ap_situation_agree")
 
 
 
