219116dba7324815a75d90d83e1133124e4cc9db
1463353697 <1463353697@qq.com>
2018-09-03 17:21:10 +0800

    js almost over

--- project1/js/project1.js
+++ project1/js/project1.js
@@ -67,10 +67,88 @@ window.onload = function(){
 		}
 
 	}
-	//获取审核表格单位
+	//获取审核表格
 	var applyHoliday  =  document.getElementById('applyholidaytab');
-	var applyHolidayTab = applyHoliday.getElementsByTagName("tr");
+	// var applyHolidayTab = applyHoliday.getElementsByTagName("tr");
 	var applyOut = document.getElementById('applyouttab');
+	// var applyOutTab = applyOut.getElementsByTagName('tr');
+	// //获取显示详细信息的input表单
+	// var holidayIfInput = document.getElementById('applyholidayif').getElementsByTagName('input');
+	// var outIfInput = document.getElementById('applyoutif').getElementsByTagName('input');
+	// //获取显示详细信息的整个页面
+	// var $applyHolidayIf = $("#applyholidayif");
+	// var $applyOutIf = $("#applyoutif");
+	// //获取返回结果的按钮
+	// var holidayResult = document.getElementById('holiday_check_result').getElementsByTagName('button');
+	// var outResult = document.getElementById('out_check_result').getElementsByTagName('button');
+ //    //获取是否紧急的按钮
+	// var jinjiIf = document.getElementById('jinji');
+
+	// addBackColor(applyHoliday);
+	// addBackColor(applyOut);
+
+	//这里是为审核部分创建表格和span标签
+	function addCheckTb(block,appendInto){
+		for (var i = 0; i < block.length; i++) {
+			var tr = document.createElement('tr');
+			appendInto.appendChild(tr);
+			var td = document.createElement('td');
+			tr.appendChild(td);
+			for (var i = 0; i < 4; i++) {
+				var span = document.createElement('span');
+				tr.appendChild(span);
+			}
+
+		}
+
+	}
+	
+
+	//请假审核
+	$.get("后台",function(){
+		addCheckTb(holidayReview,applyHoliday);
+		var tr = applyHoliday.getElementsByTagName('tr')
+		for (var i = 0; i < tr.length; i++) {
+			var td = tr[i].getElementsByTagName('td');
+			var span = td[0].getElementsByTagName('span');
+			tr[i].td[0].span[0].innerHTML = holidayReview.number;
+			tr[i].td[0].span[1].innerHTML = holidayReview.applyPerson;
+			tr[i].td[0].span[2].innerHTML = holidayReview.date;
+			tr[i].td[0].span[3].innerHTML = holidayReview.applyType;
+			//添加紧急标志
+			if (outReview[i].jinji == true) {
+				var urgent = "<span>"+"【紧急】"+"</span>";
+				tr[i].td[0].appendChild(urgent);
+			}
+
+		}
+	});
+
+	//出差审核
+	$.get("后台",function(){
+		addCheckTb(outReview,applyOut);
+		var tr = applyOut.getElementsByTagName('tr')
+		for (var i = 0; i < tr.length; i++) {
+			var td = tr[i].getElementsByTagName('td');
+			var span = td[0].getElementsByTagName('span');
+			tr[i].td[0].span[0].innerHTML = outReview.number;
+			tr[i].td[0].span[1].innerHTML = outReview.applyPerson;
+			tr[i].td[0].span[2].innerHTML = outReview.date;
+			tr[i].td[0].span[3].innerHTML = outReview.applyReason;
+
+			//添加紧急标志
+			if (outReview[i].jinji == true) {
+				var urgent = "<span>"+"【紧急】"+"</span>";
+				tr[i].td[0].appendChild(urgent);
+			}
+
+		}
+	});
+
+
+
+
+	var applyHolidayTab = applyHoliday.getElementsByTagName("tr");
 	var applyOutTab = applyOut.getElementsByTagName('tr');
 	//获取显示详细信息的input表单
 	var holidayIfInput = document.getElementById('applyholidayif').getElementsByTagName('input');
@@ -88,51 +166,11 @@ window.onload = function(){
 	addBackColor(applyOut);
 
 
+
+
 //这是一个测试的语句
 // holidayIfInput[0].value = "he";
 
-//给每个单位表格添加点击事件，点击之后，还要把各种信息添加进去，
-//还要实现同意、暂不处理、驳回的处理结果，并关闭信息页面
-	// for (var i = 0; i < applyHolidayTab.length; i++) {
-	// 	applyHolidayTab[i].onclick = function(){
-	// 		var $applyHolidayIf = $("#applyholidayif");
-	// 		$applyHolidayIf.css("display" , "block");
-	// 		$.get("后台",function(){
-	// 			holidayIfInput[0].value = holidayReview[i].applyPerson;
-	// 			holidayIfInput[1].value = holidayReview[i].number;
-	// 			holidayIfInput[2].value = holidayReview[i].department;
-	// 			holidayIfInput[3].value = holidayReview[i].startTime;
-	// 			holidayIfInput[4].value = holidayReview[i].endTime;
-	// 			holidayIfInput[5].value = holidayReview[i].applyType;
-	// 		//给表示处理结果的按钮添加点击事件，返回处理结果并关闭页面，这里可以尝试封装一个函数
-	// 			var result = document.getElementById('holiday_check_result').getElementsByTagName('button');
-	// 			result[0].onclick = function(){
-	// 				$.post("后台",
-	// 					holidayReview.result = "同意",function(){
-	// 						$applyHolidayIf.css("display" , "none");
-	// 					}
-	// 				)
-	// 			}
-	// 			result[1].onclick = function(){
-	// 				$.post("后台",
-	// 					holidayReview.result = "驳回",function(){
-	// 						$applyHolidayIf.css("display" , "none");
-	// 					}
-	// 				)
-	// 			}
-	// 			result[2].onclick = function(){
-	// 				$.post("后台",
-	// 					holidayReview.result = "未处理",function(){
-	// 						$applyHolidayIf.css("display" , "none");
-	// 					}
-	// 				)
-	// 			}
-	// 		}
-
-
-	// 	)
-	// 	}
-	// }
 
 	
 
@@ -236,61 +274,18 @@ window.onload = function(){
 
 
 
+	//这个函数是为相应的板块创建tr和td标签
+	function creatTrandTd(block,appendInto,tdNumber){
+		for (var i = 0; i < block.length; i++) {
+		var tr = document.createElement('tr');
+		appendInto.appendChild(tr);
+			for (var i = 0; i < tdNumber; i++) {
+				var td = document.createElement('td');
+				tr.appendChild(td);
+			}
+		}
 
-//这里是一个封装失败的函数
-// function applyTabClick (applyTabId,applyIfId){
-// 	var applyTab = document.getElementById('applyTabId').getElementsByTagName('tr');
-
-// 	for (var i = 0; i < applyTab.length; i++) {
-// 		applyTab[i].onclick = function(){
-// 			var $applyIf = $("#applyIfId");
-// 			$applyIf.css("display" , "block");
-// 		}
-// 	}
-// }
-// applyTabClick(applyholidaytab,applyholidayif);
-// applyTabClick(applyouttab,applyoutif);
-
-
-// //创建i个tr标签，然后再每个tr标签里创建4个td标签
-// for (var i = 0; i < applyProgress.length; i++) {
-// 	var tr = document.createElement('tr');
-// 	applyProTab.appendChild(applyProTab);
-// 	for (var i = 0; i < 4; i++) {
-// 		var td = document.createElement('td');
-// 		tr.appendChild(td);
-// 	}
-// }
-
-//这个函数是为相应的板块创建tr和td标签
-function creatTrandTd(block,appendInto,tdNumber){
-	for (var i = 0; i < block.length; i++) {
-	var tr = document.createElement('tr');
-	applyProTab.appendChild(tr);
-	for (var i = 0; i < tdNumber; i++) {
-		var td = document.createElement('td');
-		tr.appendChild(td);
 	}
-}
-
-}
-
-
-tr[i].td[0].innerHTML = applyProgress[i].date;
-tr[i].td[1].innerHTML = applyProgress[i].applyType;
-tr[i].td[3].innerHTML = applyProgress[i].applySituation;
-tr[i].td[4].innerHTML = applyProgress[i].audit;
-
-
-
-
-
-
-
-
-
-
-
 
      //申请进度
 
@@ -299,6 +294,7 @@ tr[i].td[4].innerHTML = applyProgress[i].audit;
 		creatTrandTd(applyProgress,applyProTab,4);
 		var tr = applyProTab.getElementsByTagName('tr');
 		for (var i = 0; i < tr.length; i++) {
+			var td = tr[i+1].getElementsByTagName('td');
 			tr[i+1].td[0].innerHTML = applyProgress[i].date;
 			tr[i+1].td[1].innerHTML = applyProgress[i].applyType;
 			tr[i+1].td[2].innerHTML = applyProgress[i].applySituation;
@@ -306,7 +302,6 @@ tr[i].td[4].innerHTML = applyProgress[i].audit;
 
 		}
 	});
-
 
 	addColor(applyProTab,"驳回","appro_situation_oppose","同意","appro_situation_agree","未处理","appro_situation_wait");
 	addBackColor(applyProTab);
@@ -317,6 +312,7 @@ tr[i].td[4].innerHTML = applyProgress[i].audit;
 		creatTrandTd(dakaSituation,cardStuaTab,6);
 		var tr = cardStuaTab.getElementsByTagName('tr');
 		for (var i = 0; i < tr.length; i++) {
+			var td = tr[i+1].getElementsByTagName('td');
 			tr[i+1].td[0].innerHTML = dakaSituation[i].number;
 			tr[i+1].td[1].innerHTML = dakaSituation[i].date;
 			tr[i+1].td[2].innerHTML = dakaSituation[i].userName;
@@ -336,6 +332,7 @@ tr[i].td[4].innerHTML = applyProgress[i].audit;
 		creatTrandTd(leaveSituation,hliStuaTab,7);
 		var tr = hliStuaTab.getElementsByTagName('tr');
 		for (var i = 0; i < tr.length; i++) {
+			var td = tr[i+1].getElementsByTagName('td');
 			tr[i+1].td[0].innerHTML = leaveSituation[i].number;
 			tr[i+1].td[1].innerHTML = leaveSituation[i].date;
 			tr[i+1].td[2].innerHTML = leaveSituation[i].userName;
@@ -347,11 +344,14 @@ tr[i].td[4].innerHTML = applyProgress[i].audit;
 	})
 	addBackColor(hliStuaTab);
 	addColor(hliStuaTab,"驳回","ap_situation_oppose","未处理","ap_situation_wait","通过","ap_situation_agree");
+
+
 	//出差情况
 	var outStuaTab = document.getElementById('out_stuatb');
 	$.get(后台,function(){
 		creatTrandTd(awaySituation,outStuaTab,7);
 		for (var i = 0; i < tr.length; i++) {
+			var td = tr[i+1].getElementsByTagName('td');
 			tr[i+1].td[0].innerHTML = leaveSituation[i].number;
 			tr[i+1].td[1].innerHTML = leaveSituation[i].date;
 			tr[i+1].td[2].innerHTML = leaveSituation[i].userName;
@@ -361,7 +361,8 @@ tr[i].td[4].innerHTML = applyProgress[i].audit;
 			tr[i+1].td[6].innerHTML = leaveSituation[i].resultHand;
 		}
 
-	})
+	});
+
 	addBackColor(outStuaTab);
 	addColor(outStuaTab,"驳回","ap_situation_oppose","未处理","ap_situation_wait","通过","ap_situation_agree")
 
